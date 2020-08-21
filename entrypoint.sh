d3cfcea3e8607b16d86a8f9b42a68c28edd49a75
maged dev <maged.dev@gmail.com>
2020-08-21 07:56:49 +0200

    works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+groupadd www
+usermod -a -G www ubuntu
+
+mkdir /etc/resty-auto-ssl
+
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 \
+  -subj '/CN=sni-support-required-for-valid-ssl' \
+  -keyout /etc/ssl/resty-auto-ssl-fallback.key \
+  -out /etc/ssl/resty-auto-ssl-fallback.crt
+
+chmod -R 777 /etc/resty-auto-ssl 
+chmod -R 777 /usr/local/openresty
+chmod -R 777 /etc/ssl/
+
 pkill openresty
 pkill nginx
 openresty -c /home/conf/nginx.conf
